7bef7aa0c63b83f705871bd161ff3e27ec3435b9
Blobface525 <78133680+Blobface525@users.noreply.github.com>
2021-01-28 00:58:59 -0500

    Add files via upload
    Added cs

---
+++ ArtifactOfTheKing.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using RoR2;
+using RoR2.Projectile;
+using EntityStates.LunarGolem;
+using EntityStates.LunarWisp;
+using EntityStates.BrotherMonster;
+using EntityStates.BrotherMonster.Weapon;
+using Unity;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using R2API;
+using R2API.Utils;
+using EntityStates;
+
+
+namespace Blobface
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Blobface.ArtifactKing", "Artifact of the King", "1.0.0")]
+    public class ArtifactKing : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.EntityStates.BrotherMonster.BaseSlideState.OnEnter += (orig, self) =>
+            {
+                BaseState RefBase = new BaseState();
+                var DamStat = RefBase.GetFieldValue<float>("damageStat");
+                Ray aimRay = RefBase.InvokeMethod<Ray>("GetAimRay");
+                ProjectileManager.instance.FireProjectile(FireTwinShots.projectilePrefab, aimRay.origin, Util.QuaternionSafeLookRotation(aimRay.direction), base.gameObject, RefBase.GetFieldValue<float>("damageStat") * FireTwinShots.damageCoefficient, FireTwinShots.force, Util.CheckRoll(RefBase.GetFieldValue<float>("critStat"), RefBase.GetPropertyValue<int>("master")), DamageColorIndex.Default, null, -1f);
+                orig(self);
+            };
+        }
+    }
+}
